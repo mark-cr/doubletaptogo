fb9c85cb5ed1ca6808910fe4b42b4ef2d1f6e99a
Zeno Popovici <myself@zeno.ro>
2014-09-09 18:28:30 +0300

    Cleanup spacing.

--- jquery.doubletaptogo.js
+++ jquery.doubletaptogo.js
@@ -3,43 +3,35 @@
 	Available for use under the MIT License
 */
 
-
-
-;(function( $, window, document, undefined )
-{
-	$.fn.doubleTapToGo = function( params )
-	{
-		if( !( 'ontouchstart' in window ) &&
+;(function($, window, document, undefined) {
+	$.fn.doubleTapToGo = function(params) {
+		if (!('ontouchstart' in window) &&
 			!navigator.msMaxTouchPoints &&
-			!navigator.userAgent.toLowerCase().match( /windows phone os 7/i ) ) return false;
+			!navigator.userAgent.toLowerCase().match( /windows phone os 7/i )) return false;
 
-		this.each( function()
-		{
+		this.each(function() {
 			var curItem = false;
 
-			$( this ).on( 'click', function( e )
-			{
-				var item = $( this );
-				if( item[ 0 ] != curItem[ 0 ] )
-				{
+			$(this).on('click', function(e) {
+				var item = $(this);
+				if (item[0] != curItem[0]) {
 					e.preventDefault();
 					curItem = item;
 				}
 			});
 
-			$( document ).on( 'click touchstart MSPointerDown', function( e )
-			{
+			$(document).on('click touchstart MSPointerDown', function(e) {
 				var resetItem = true,
-					parents	  = $( e.target ).parents();
+					parents = $(e.target).parents();
 
-				for( var i = 0; i < parents.length; i++ )
-					if( parents[ i ] == curItem[ 0 ] )
+				for (var i = 0; i < parents.length; i++)
+					if (parents[i] == curItem[0])
 						resetItem = false;
 
-				if( resetItem )
+				if(resetItem)
 					curItem = false;
 			});
 		});
 		return this;
 	};
-})( jQuery, window, document );
+})(jQuery, window, document);
